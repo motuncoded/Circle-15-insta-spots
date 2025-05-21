1b9fb862bbd6afaf034f72fcaf109b7a6145ac57
BaaWA <bmdebell32@gmsil.com>
2025-05-21 22:11:41 +0100

    Refactor file structure and add new features:
    1. Render cards from an array of objects
    
    2. Make like icons functional
    
    3. Add saving and retrieving from local storage

---
+++ data.js
@@ -0,0 +1,32 @@
+export const cardDetails = [
+  {
+    name: "Val Thorens",
+    imgSrc: "./assets/val-thorens.svg",
+    liked: false,
+  },
+  {
+    name: "Restaurant terrace",
+    imgSrc: "./assets/restaurant-terrace.svg",
+    liked: false,
+  },
+  {
+    name: "An outdoor cafe",
+    imgSrc: "./assets/an-outdoor-cafe.svg",
+    liked: false,
+  },
+  {
+    name: "A very long bridge, over the forest...",
+    imgSrc: "./assets/mask-group-3.png",
+    liked: false,
+  },
+  {
+    name: "Tunnel with morning light",
+    imgSrc: "./assets/mask-group-4.png",
+    liked: false,
+  },
+  {
+    name: "Mountain house",
+    imgSrc: "./assets/mask-group-6.png",
+    liked: false,
+  },
+];
